52c3dae977c3094a42379fa318cd2e62005ca614
bboysquiz <mrkiraller@gmail.com>
2023-10-12 12:50:25 +0300

    added types

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import axios, { AxiosResponse } from 'axios'
 import { reactive } from 'vue'
+import { Data, JokeApiResponse } from '../types'
 
 const data: Data = reactive({
     jokes: []
@@ -10,13 +11,11 @@ export const useJokesStore = defineStore('jokes', () => {
     const getJokes = async (searchingText: string): Promise<void> => {
         try {
             const url = `https://api.chucknorris.io/jokes/search?query=${searchingText}`;
-            const response: AxiosResponse<Jokes> = await axios.get(url);
+            const response: AxiosResponse<JokeApiResponse> = await axios.get(url);
             data.jokes = response.data.result;
-            console.log(data.jokes)
-            data.error = null
+            console.log(response)
         } catch (error: any) {
             console.error(error.response.data.message);
-            data.error = error.response.data.message;
         }
     };
     return { data, getJokes }
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,18 @@
-export interface Jokes [
-    
-]
+export interface Joke {
+    categories: [],
+    created_at: string,
+    icon_url: string,
+    id: string,
+    updated_at: string,
+    url: string,
+    value: string,
+}
+
+export interface Data {
+    jokes: Joke[],
+}
+
+export interface JokeApiResponse {
+    total: number;
+    result: Joke[];
+}
