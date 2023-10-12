e5f00e6c4d6d5bd0f016c184c03810e4e134e903
bboysquiz <mrkiraller@gmail.com>
2023-10-12 13:14:04 +0300

    added styles for search input

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -14,8 +14,10 @@ const handleInput = async () => {
 
 <template>
     <div class="search__container">
-        <input class="search__input" v-model="query" @input='handleInput' placeholder="Search for a joke..." autofocus />
-        <div class="search__jokes-conter">{{ data.jokes.length }}</div>
+        <div class="search__input-wrapper">
+            <input class="search__input" v-model="query" @input='handleInput' placeholder="Some value" autofocus />
+            <div class="search__jokes-conter">Found jokes: {{ data.jokes.length }}</div>
+        </div>
         <div class="search__block-wrapper">
             <div class="search__block" v-for="joke in data.jokes" :key="joke.id">
                 <p class="search__joke-text">{{ joke.value }}</p>
@@ -26,4 +28,61 @@ const handleInput = async () => {
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.search__container {
+    width: 1596px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 128px auto 0 auto;
+}
+.search__input-wrapper {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    width: 626px;
+}
+
+.search__input {
+    padding-left: 36px;
+    width: 626px;
+    height: 64px;
+    box-shadow: 0px 7px 12px 0px rgba(100, 100, 111, 0.2);
+    font-family: Fira Sans;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24px;
+    letter-spacing: 0px;
+    color: #656EC2;
+    border: none;
+}
+
+.search__input::placeholder {
+    color: #656EC2;
+}
+
+.search__input::-moz-placeholder {
+    color: #656EC2;
+}
+
+.search__input:-ms-input-placeholder {
+    color: #656EC2;
+}
+
+.search__input::-webkit-input-placeholder {
+    color: #656EC2;
+}
+
+.search__jokes-conter {
+    font-family: Montserrat, sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+    letter-spacing: 0px;
+    color: #282626;
+    width: 100%;
+    padding-left: 36px;
+    padding-top: 20px;
+    align-items: center;
+    display: flex;
+}</style>
